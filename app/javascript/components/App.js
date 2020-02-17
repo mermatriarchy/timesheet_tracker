ed5d7494dc9b66ef6545801b5188d5329653bf6d
mermatriarchy <hmiller@laurelroad.com>
2020-02-17 13:28:58 -0500

    add table headers

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,25 +2,33 @@ import React, { useState, useEffect } from 'react';
 import TimesheetTable from './TimesheetTable';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Row } from 'react-bootstrap';
 
 export default function App() {
   const [entries, setEntries] = useState([]);
+  const [sortByProject, setSortBy] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        '/api/v1/timesheet_entries',
-      );
+      const apiUrl = sortByProject ? '/api/v1/timesheet_entries?sort_by_project=true' : '/api/v1/timesheet_entries';
+      const result = await axios(apiUrl);
       setEntries(result.data);
     };
     fetchData();
   }, []);
 
+  const handleSort = () => {
+    setSortBy(!sortByProject);
+  }
+
   return(
-      <div>
-          <div>Hello World!!!</div>
-          <TimesheetTable entries={entries}/>
-      </div>
+      <Container>
+          <h1>Timesheet Tracker</h1>
+          <Row>
+            <Button onClick={handleSort}>{sortByProject ? 'View All Entries' : 'Sort By Project'}</Button>
+          </Row>
+          <TimesheetTable entries={entries} sortByProject={sortByProject}/>
+      </Container>
   )
     
 }
--- app/javascript/components/TimesheetTable.js
+++ app/javascript/components/TimesheetTable.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react';
 import TimesheetEntry from './TimesheetEntry';
-import axios from 'axios';
+import { Table } from 'react-bootstrap';
 
 export default function TimesheetTable(props) {
         return(
-            props.entries.map((entry) => {
-                return(
-                  <div key={entry.id}>
-                   <TimesheetEntry entry={entry}/>
-                  </div>
-                )
-            })
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                {
+                  props.sortByProject ? (
+                  <>
+                    <th>Name</th>
+                    <th>Client</th>
+                    <th>Hours</th>
+                    <th>Billable Hours</th>
+                    <th>Billable Amount</th>
+                  </>
+                  ) : (
+                  <>
+                    <th>Project Name</th>
+                    <th>Client</th>
+                    <th>Hours</th>
+                    <th>Billable Hours</th>
+                    <th>Contributor Name</th>
+                    <th>Billable Rate</th>
+                  </>
+                  )
+                }
+                </tr>
+              </thead>
+              <tbody>
+                {props.entries.map((entry, index) => {
+                  return(
+                    <div key={props.sortByProject ? index : entry.id}>
+                     <TimesheetEntry entry={entry}/>
+                    </div>
+                  )
+                })}
+              </tbody>
+            </Table>
         )
 }
