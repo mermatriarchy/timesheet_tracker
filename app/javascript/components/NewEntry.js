c37cb13cff8260af74641184200698c465e9b780
mermatriarchy <hmiller@laurelroad.com>
2020-02-18 18:28:48 -0500

    refactor form, convert to class component

--- app/javascript/components/NewEntry.js
+++ app/javascript/components/NewEntry.js
@@ -1,113 +1,96 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Container, Col, Form } from 'react-bootstrap';
 import axios from 'axios';
 
-export default function NewEntry(props) {
-  const [clientName, setClientName] = useState('');
-  const [projectName, setProjectName] = useState('');
-  const [projectCode, setProjectCode] = useState('');
-  const [date, setDate] = useState('');
-  const [contribFirstName, setContribFirstName] = useState('');
-  const [contribLastName, setContribLastName] = useState('');
-  const [hours, setHours] = useState(0);
-  const [billableRate, setBillableRate] = useState(0);
-
-  const handleUserInput = (event) => {
-    const fieldName = event.target.name;
-    const value = event.target.value;
-
-    switch(fieldName) {
-      case "clientName":
-        setClientName(value);
-        break;
-      case "projectName":
-        setProjectName(value);
-        break;
-      case "projectCode":
-        setProjectCode(value);
-      break;
-      case "date":
-        setDate(value);
-      break;
-      case "contribFirstName":
-        setContribFirstName(value);
-      break;
-      case "contribLastName":
-        setContribLastName(value);
-      break;
-      case "hours":
-        setHours(value);
-      break;
-      case "billableRate":
-        setBillableRate(value);
-      break;
-      default:
-      break;
+class NewEntry extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      clientName: '',
+      projectName: '',
+      projectCode: '',
+      date: '',
+      contribFirstName: '',
+      contribLastName: '',
+      hours: 0,
+      billableRate: 0,
     }
   }
 
-  const handleSubmit = (event) => {
+  handleUserInput = (event) => {
+    const {name, value} = event.target;
+
+    this.setState((state, props) => (
+      { [name]: value})
+      );
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault();
+    let self = this;
 
     axios.post('/api/v1/timesheet_entries', {
-       client_name: clientName,
-       project_name: projectName,
-       project_code: projectCode,
-       date: date,
-       contributor_first_name: contribFirstName,
-       contributor_last_name: contribLastName,
-       hours: hours,
-       billable: billableRate > 0 ? true : false,
-       billable_rate: billableRate
+       client_name: this.state.clientName,
+       project_name: this.state.projectName,
+       project_code: this.state.projectCode,
+       date: this.state.date,
+       contributor_first_name: this.state.contribFirstName,
+       contributor_last_name: this.state.contribLastName,
+       hours: this.state.hours,
+       billable: this.state.billableRate > 0 ? true : false,
+       billable_rate: this.state.billableRate
       })
       .then(function (response) {
-        props.handleNewEntry(response);
+        self.props.handleNewEntry(response);
       })
       .catch(function (error) {
         console.log(error);
       });
   }
-
-  return(
-      <Container>
-        <h2>Add New Timesheet Entry</h2>
-        <Form  onSubmit={handleSubmit}>
-          <Form.Row>
-            <Col>
-              <Form.Control placeholder="Client name" type="text" name="clientName" value={clientName} onChange={handleUserInput}/>
-            </Col>
-            <Col>
-              <Form.Control placeholder="Project name" type="text" name="projectName" value={projectName} onChange={handleUserInput}/>
-            </Col>
-            <Col>
-              <Form.Control placeholder="Project code" type="text" name="projectCode" value={projectCode} onChange={handleUserInput}/>
-            </Col>
-            <Col>
-              <Form.Control placeholder="Date" type="date" name="date" value={date} onChange={handleUserInput}/>
-            </Col>
-          </Form.Row>
-          <Form.Row>
-            <Col>
-              <Form.Control placeholder="Contributor first name" type="text" name="contribFirstName" value={contribFirstName} onChange={handleUserInput}/>
-            </Col>
-            <Col>
-              <Form.Control placeholder="Contibutor last name" type="text" name="contribLastName" value={contribLastName} onChange={handleUserInput}/>
-            </Col>
-            <Col>
-              <Form.Control placeholder="Hours" type="number" name="hours" value={hours} onChange={handleUserInput}/>
-            </Col>
-            <Col>
-              <Form.Control placeholder="Billable rate (enter 0 if item is not billable)" type="number" name="billableRate" value={billableRate} onChange={handleUserInput}/>
-            </Col>
-          </Form.Row>
-          <Form.Row>
-            <Col md={{span: 2, offset: 10}}>
-              <Button type="submit">
-                Submit Entry
-              </Button>
-            </Col>
-          </Form.Row>
-        </Form>
-      </Container>  
-  )
+  render(){
+    const { clientName, projectName, projectCode, date, contribFirstName, contribLastName, hours, billableRate} = this.state;
+    return(
+        <Container>
+          <h2>Add New Timesheet Entry</h2>
+          <Form  onSubmit={this.handleSubmit}>
+            <Form.Row>
+              <Col>
+                <Form.Control placeholder="Client name" type="text" name="clientName" value={clientName} onChange={this.handleUserInput}/>
+              </Col>
+              <Col>
+                <Form.Control placeholder="Project name" type="text" name="projectName" value={projectName} onChange={this.handleUserInput}/>
+              </Col>
+              <Col>
+                <Form.Control placeholder="Project code" type="text" name="projectCode" value={projectCode} onChange={this.handleUserInput}/>
+              </Col>
+              <Col>
+                <Form.Control placeholder="Date" type="date" name="date" value={date} onChange={this.handleUserInput}/>
+              </Col>
+            </Form.Row>
+            <Form.Row>
+              <Col>
+                <Form.Control placeholder="Contributor first name" type="text" name="contribFirstName" value={contribFirstName} onChange={this.handleUserInput}/>
+              </Col>
+              <Col>
+                <Form.Control placeholder="Contibutor last name" type="text" name="contribLastName" value={contribLastName} onChange={this.handleUserInput}/>
+              </Col>
+              <Col>
+                <Form.Control placeholder="Hours" type="number" name="hours" value={hours} onChange={this.handleUserInput}/>
+              </Col>
+              <Col>
+                <Form.Control placeholder="Billable rate (enter 0 if item is not billable)" type="number" name="billableRate" value={billableRate} onChange={this.handleUserInput}/>
+              </Col>
+            </Form.Row>
+            <Form.Row>
+              <Col md={{span: 2, offset: 10}}>
+                <Button type="submit">
+                  Submit Entry
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>  
+    )
+  }
 }
+export default NewEntry;
