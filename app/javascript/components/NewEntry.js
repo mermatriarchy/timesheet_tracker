2136fbc7a2c498f3425283cc8e73bdf321fdfe5d
mermatriarchy <hmiller@laurelroad.com>
2020-02-18 18:55:02 -0500

    cleanup form layout

--- app/javascript/components/NewEntry.js
+++ app/javascript/components/NewEntry.js
@@ -47,42 +47,54 @@ class NewEntry extends React.Component {
         console.log(error);
       });
   }
+
   render(){
     const { clientName, projectName, projectCode, date, contribFirstName, contribLastName, hours, billableRate} = this.state;
     return(
         <Container>
           <h2>Add New Timesheet Entry</h2>
           <Form  onSubmit={this.handleSubmit}>
-            <Form.Row>
-              <Col>
-                <Form.Control placeholder="Client name" type="text" name="clientName" value={clientName} onChange={this.handleUserInput}/>
-              </Col>
-              <Col>
-                <Form.Control placeholder="Project name" type="text" name="projectName" value={projectName} onChange={this.handleUserInput}/>
-              </Col>
-              <Col>
-                <Form.Control placeholder="Project code" type="text" name="projectCode" value={projectCode} onChange={this.handleUserInput}/>
-              </Col>
-              <Col>
-                <Form.Control placeholder="Date" type="date" name="date" value={date} onChange={this.handleUserInput}/>
-              </Col>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Client Name</Form.Label>
+              <Form.Control placeholder="Client name" type="text" name="clientName" value={clientName} onChange={this.handleUserInput}/>
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Project Name</Form.Label>
+              <Form.Control placeholder="Project name" type="text" name="projectName" value={projectName} onChange={this.handleUserInput}/>
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Project Code</Form.Label>
+              <Form.Control placeholder="Project code" type="text" name="projectCode" value={projectCode} onChange={this.handleUserInput}/>
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Date</Form.Label>
+              <Form.Control placeholder="Date" type="date" name="date" value={date} onChange={this.handleUserInput}/>
+            </Form.Group>
             </Form.Row>
             <Form.Row>
-              <Col>
-                <Form.Control placeholder="Contributor first name" type="text" name="contribFirstName" value={contribFirstName} onChange={this.handleUserInput}/>
-              </Col>
-              <Col>
-                <Form.Control placeholder="Contibutor last name" type="text" name="contribLastName" value={contribLastName} onChange={this.handleUserInput}/>
-              </Col>
-              <Col>
-                <Form.Control placeholder="Hours" type="number" name="hours" value={hours} onChange={this.handleUserInput}/>
-              </Col>
-              <Col>
-                <Form.Control placeholder="Billable rate (enter 0 if item is not billable)" type="number" name="billableRate" value={billableRate} onChange={this.handleUserInput}/>
-              </Col>
+            <Form.Group as={Col}>
+              <Form.Label>Contributor First Name</Form.Label>
+              <Form.Control placeholder="Contributor first name" type="text" name="contribFirstName" value={contribFirstName} onChange={this.handleUserInput}/>
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Contributor Last Name</Form.Label>
+              <Form.Control placeholder="Contibutor last name" type="text" name="contribLastName" value={contribLastName} onChange={this.handleUserInput}/>
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Hours</Form.Label>
+              <Form.Control placeholder="Hours" type="number" name="hours" value={hours} onChange={this.handleUserInput}/>
+            </Form.Group>
+            <Form.Group as={Col}>
+                <Form.Label>Billable Rate</Form.Label>
+                <Form.Control placeholder="Billable rate" type="number" name="billableRate" value={billableRate} onChange={this.handleUserInput}/>
+                <Form.Text className="text-muted">
+                  Please enter 0 if entry is not billable.
+                </Form.Text>
+            </Form.Group>
             </Form.Row>
             <Form.Row>
-              <Col md={{span: 2, offset: 10}}>
+              <Col md={{offset: 10}}>
                 <Button type="submit">
                   Submit Entry
                 </Button>
